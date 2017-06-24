0abaf969076c6f2bf3ac4274ab758592b5c04c6f
yl-zeng <ylinzeng69@gmail.com>
2017-06-24 13:52:20 -0400

    UserRole, Authority, Role

--- src/main/java/com/bookstore/domain/User.java
+++ src/main/java/com/bookstore/domain/User.java
@@ -1,13 +1,27 @@
 package com.bookstore.domain;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.bookstore.domain.security.Authority;
+import com.bookstore.domain.security.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class User{
+public class User implements UserDetails{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -23,6 +37,11 @@ public class User{
 	private String phone;
 	private boolean enabled = true;
 
+	@OneToMany(mappedBy = "user",cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JsonIgnore
+	private Set<UserRole> userRoles = new HashSet<>();
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -33,6 +52,14 @@ public class User{
 
 	public String getUsername() {
 		return username;
+	}
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
 	}
 
 	public void setUsername(String username) {
@@ -78,7 +105,8 @@ public class User{
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
+	
+	@Override
 	public boolean isEnabled() {
 		return enabled;
 	}
@@ -86,5 +114,33 @@ public class User{
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		Set<GrantedAuthority> authorities = new HashSet<>();
+		userRoles.forEach(ur -> authorities.add(new Authority(ur.getRole().getName())));
+		
+		return authorities;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
 
 }
