18bbc236f998c072919a246741007f672f58ab04
yl-zeng <ylinzeng69@gmail.com>
2017-06-24 15:26:46 -0400

    Mapping for login

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.bookstore.service.impl.UserSecurityService;
+import com.bookstore.utility.SecurityUtility;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
